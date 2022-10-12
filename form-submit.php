033e65442786a959eb33b2dabe50e05b85a80cc7
Daniel Duea <danielduea@gmail.com>
2022-10-11 20:26:36 -0500

    Fix missing logos
    Fixing 2 missing logos missed in the restructuring

--- form-submit.php
+++ form-submit.php
@@ -106,7 +106,7 @@ echo "<!DOCTYPE html>
     <?php include 'show-button.php'; ?>
     <header class=\"inverse\">
       <div class=\"container\">
-        <img class =\"logo\" src=\"logo.png\" alt=\"Logo\">
+        <img class =\"logo\" src=\"images/logo.png\" alt=\"Logo\">
         <h1> <span class=\"accent-text\">Registration Form</span></h1>
       </div>
       <div class=\"navbar\">
