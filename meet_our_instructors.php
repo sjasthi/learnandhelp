55d1b1be09d887581d1b6c3adc8ff23c4d29b317
Luis Duran-Enriquez <89470754+racingturtle12@users.noreply.github.com>
2022-10-26 08:30:41 -0500

    changed logo
    added logo in navbar and in all places where old one was

--- meet_our_instructors.php
+++ meet_our_instructors.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <link rel="icon" href="images/logo.png" type="image/icon type">
+    <link rel="icon" href="images/new_logo.png" type="image/icon type">
     <title>Learn and Help</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;900&display=swap" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
@@ -10,10 +10,11 @@
   <?php include 'show-button.php'; ?>
   <header class="inverse">
       <div class="container">
-        <img class ="logo" src="images/logo.png" alt="Logo">
+        <img class ="new_logo" src="images/new_logo.png" alt="Logo">
         <h1>Meet our<span class="accent-text">Instructors</span></h1>
       </div>
       <div class="navbar">
+      <a href="homepage.php"><img class="nav_new_logo" src="images/new_logo.png"></a>
           <a href="homepage.php">Home</a>
           <a href="#">Instructors and Volunteers Sign Up</a>
           <a href="#">Classes</a>
