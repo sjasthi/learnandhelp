7b08fa6cdc62657603ec48730b00ffdd9c89c4e3
Daniel Duea <danielduea@gmail.com>
2022-11-15 15:23:12 -0600

    Add error warning on page when login validation fails
    Instead of creating an alert the login page now warns the user that their login is invalid on the page

--- validate-login.php
+++ validate-login.php
@@ -1,3 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="icon" href="images/new_logo.png" type="image/icon type">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;900&display=swap" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+  </head>
+  <body>
+  <?php include 'show-navbar.php'; ?>
+  <?php show_navbar(); ?>
+  <header class="inverse">
+      <div class="container">
+          <h1><span class="accent-text">Login</span></h1>
+      </div>
+  </header>
+  <p style='font-weight: bold'>Invalid Username or Password</p>
+  <br>
+  <form action="validate-login.php" method="post">
+      <label for="usermail">Email</label>
+      <br>
+      <input id="usermail" type="email" name="usermail" placeholder="Yourname@email.com" required>
+      <br>
+      <label for="password">Password</label>
+      <br>
+      <input id="password" type="password" name="password" placeholder="Password" required>
+      <br>
+      <input type="submit" id="submit-login" value="Login"/>
+	</form>
+  <a href="create_account.php" id="create_account_button">Create Account</button>
+  </body>
+</html>
+
 <?php
 
 $status = session_status();
@@ -5,35 +38,32 @@ if ($status == PHP_SESSION_NONE) {
   session_start();
 }
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "learn_and_help_db";
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "learn_and_help_db";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
 
-// Escape inputs to prevent SQL injection
-$usermail = $conn->escape_string($_POST["usermail"]);
-$account_password = $conn->escape_string($_POST["password"]);
-$hash = sha1($account_password);
+  // Escape inputs to prevent SQL injection
+  $usermail = $conn->escape_string($_POST["usermail"]);
+  $account_password = $conn->escape_string($_POST["password"]);
+  $hash = sha1($account_password);
 
-$sql = "SELECT Role, First_Name, User_Id FROM users WHERE Email='$usermail' AND Hash='$hash';";
-$result = $conn->query($sql);
+  $sql = "SELECT Role, First_Name, User_Id FROM users WHERE Email='$usermail' AND Hash='$hash';";
+  $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    $entry = $result -> fetch_assoc();
-    $_SESSION["email"] = $usermail;
-    $_SESSION["role"] = $entry['Role'];
-    $_SESSION["first_name"] = $entry['First_Name'];
-    $_SESSION["User_Id"] = $entry['User_Id'];
-    header('Location: homepage.php');
-} else {
-    echo "<script type='text/javascript'>alert('Invalid username or password');</script>";
-}
-$conn->close();
+  if ($result->num_rows > 0) {
+      $entry = $result -> fetch_assoc();
+      $_SESSION["email"] = $usermail;
+      $_SESSION["role"] = $entry['Role'];
+      $_SESSION["first_name"] = $entry['First_Name'];
+      $_SESSION["User_Id"] = $entry['User_Id'];
+      header('Location: homepage.php');
+  }
 ?>
