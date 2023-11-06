5bee383b919ace3eb696b8fc3494a162eadfb71f
Abdulkader A Abdi <69095293+aaabdulakader@users.noreply.github.com>
2023-11-06 17:51:16 -0600

    Fixed API response Format

--- API/get_book_info.php
+++ API/get_book_info.php
@@ -11,57 +11,68 @@ $username = 'root';
 $password = ''; 
 $database = 'learn_and_help_db'; 
 
-
+// Initialize the response array
+$response = array();
 
 // Check if the 'id' parameter is provided in the request
 if (isset($_GET['id'])) {
     // Get the book ID from the request
     $bookId = $_GET['id'];
 
-// Create a new MySQLi object for the database connection
-$connection = new mysqli($hostname, $username, $password, $database);
+    // Create a new MySQLi object for the database connection
+    $connection = new mysqli($hostname, $username, $password, $database);
 
     // Check for a successful database connection
     if ($connection->connect_error) {
-        die("Failed to connect to the database: " . $connection->connect_error);
-    }
-
-    // Prepare an SQL statement to retrieve book information by ID
-    $sql = "SELECT * FROM books WHERE id = ?";
-
-    // Use prepared statements to prevent SQL injection
-    $stmt = $connection->prepare($sql);
-
-    if ($stmt === false) {
-        die("Failed to prepare the statement: " . $connection->error);
-    }
-
-    // Bind the parameter and execute the statement
-    $stmt->bind_param("s", $bookId);
-
-    if ($stmt->execute()) {
-        // Fetch the result
-        $result = $stmt->get_result();
-
-        // Check if a record was found
-        if ($result->num_rows > 0) {
-            // Fetch the book data as an associative array
-            $bookData = $result->fetch_assoc();
-            echo json_encode($bookData);
-        } else {
-            // Book not found and the error code like 200, 404, 500
-            echo json_encode(array("error" => "Book not found"));
-        }
+        $response['response_code'] = 500;
+        $response['message'] = "Failed to connect to the database: " . $connection->connect_error;
     } else {
-        // Error executing the statement
-        echo json_encode(array("error" => "Database error"));
-    }
+        // Prepare an SQL statement to retrieve book information by ID
+        $sql = "SELECT * FROM books WHERE id = ?";
 
-    // Close the database connection
-    $stmt->close();
-    $connection->close();
+        // Use prepared statements to prevent SQL injection
+        $stmt = $connection->prepare($sql);
+
+        if ($stmt === false) {
+            $response['response_code'] = 500;
+            $response['message'] = "Failed to prepare the statement: " . $connection->error;
+        } else {
+            // Bind the parameter and execute the statement
+            $stmt->bind_param("s", $bookId);
+
+            if ($stmt->execute()) {
+                // Fetch the result
+                $result = $stmt->get_result();
+
+                // Check if a record was found
+                if ($result->num_rows > 0) {
+                    // Fetch the book data as an associative array
+                    $bookData = $result->fetch_assoc();
+                    $response['response_code'] = 200;
+                    $response['message'] = "Dress found";
+                    $response['data'] = $bookData;
+                } else {
+                    // Book not found
+                    $response['response_code'] = 404;
+                    $response['message'] = "Book not found";
+                }
+            } else {
+                // Error executing the statement
+                $response['response_code'] = 500;
+                $response['message'] = "Database error";
+            }
+
+            // Close the database connection
+            $stmt->close();
+        }
+        $connection->close();
+    }
 } else {
     // 'id' parameter is missing
-    echo json_encode(array("error" => "Missing 'id' parameter"));
+    $response['response_code'] = 400;
+    $response['message'] = "Missing 'id' parameter";
 }
+
+// Return the response as JSON
+echo json_encode($response);
 ?>
--- API/get_school_info.php
+++ API/get_school_info.php
@@ -9,57 +9,70 @@ error_reporting(E_ALL);
 $hostname = 'localhost'; 
 $username = 'root'; 
 $password = ''; 
-$database = 'learn_and_help_db';
+$database = 'learn_and_help_db'; 
+
+// Initialize the response array
+$response = array();
 
 // Check if the 'id' parameter is provided in the request
 if (isset($_GET['id'])) {
     // Get the school ID from the request
     $schoolId = $_GET['id'];
 
-// Create a new MySQLi object for the database connection
-$connection = new mysqli($hostname, $username, $password, $database);
+    // Create a new MySQLi object for the database connection
+    $connection = new mysqli($hostname, $username, $password, $database);
 
     // Check for a successful database connection
     if ($connection->connect_error) {
-        die("Failed to connect to the database: " . $connection->connect_error);
-    }
-
-    // Prepare an SQL statement to retrieve school information by ID
-    $sql = "SELECT * FROM schools WHERE id = ?";
-
-    // Use prepared statements to prevent SQL injection
-    $stmt = $connection->prepare($sql);
-
-    if ($stmt === false) {
-        die("Failed to prepare the statement: " . $connection->error);
-    }
-
-    // Bind the parameter and execute the statement
-    $stmt->bind_param("s", $schoolId);
-
-    if ($stmt->execute()) {
-        // Fetch the result
-        $result = $stmt->get_result();
-
-        // Check if a record was found
-        if ($result->num_rows > 0) {
-            // Fetch the school data as an associative array
-            $schoolData = $result->fetch_assoc();
-            echo json_encode($schoolData);
-        } else {
-            // school not found
-            echo json_encode(array("error" => "school not found"));
-        }
+        $response['response_code'] = 500;
+        $response['message'] = "Failed to connect to the database: " . $connection->connect_error;
     } else {
-        // Error executing the statement
-        echo json_encode(array("error" => "Database error"));
-    }
+        // Prepare an SQL statement to retrieve school information by ID
+        $sql = "SELECT * FROM schools WHERE id = ?";
 
-    // Close the database connection
-    $stmt->close();
-    $connection->close();
+        // Use prepared statements to prevent SQL injection
+        $stmt = $connection->prepare($sql);
+
+        if ($stmt === false) {
+            $response['response_code'] = 500;
+            $response['message'] = "Failed to prepare the statement: " . $connection->error;
+        } else {
+            // Bind the parameter and execute the statement
+            $stmt->bind_param("s", $schoolId);
+
+            if ($stmt->execute()) {
+                // Fetch the result
+                $result = $stmt->get_result();
+
+                // Check if a record was found
+                if ($result->num_rows > 0) {
+                    // Fetch the school data as an associative array
+                    $schoolData = $result->fetch_assoc();
+                    $response['response_code'] = 200;
+                    $response['message'] = "School found";
+                    $response['data'] = $schoolData;
+                } else {
+                    // School not found
+                    $response['response_code'] = 404;
+                    $response['message'] = "School not found";
+                }
+            } else {
+                // Error executing the statement
+                $response['response_code'] = 500;
+                $response['message'] = "Database error";
+            }
+
+            // Close the database connection
+            $stmt->close();
+        }
+        $connection->close();
+    }
 } else {
     // 'id' parameter is missing
-    echo json_encode(array("error" => "Missing 'id' parameter"));
+    $response['response_code'] = 400;
+    $response['message'] = "Missing 'id' parameter";
 }
+
+// Return the response as JSON
+echo json_encode($response);
 ?>
