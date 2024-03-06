14a48813541c95c145405afa1df603bc40d0169c
Pong <pongeslee@gmail.com>
2024-03-05 18:40:27 -0600

    Added password matching
    in file new_password_entry.php added source code to match new password.

--- new_password_entry.php
+++ new_password_entry.php
@@ -5,7 +5,6 @@
   }
  ?>
  
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -13,6 +12,18 @@
     <title>Login</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;900&display=swap" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
+    <script>
+      var check = function() {
+        if (document.getElementById('password1').value == document.getElementById('password2').value) {
+          document.getElementById('message').style.color = 'green';
+          document.getElementById('message').innerHTML = 'Password match';
+        } else {
+            document.getElementById('message').style.color = 'red';
+            document.getElementById('message').innerHTML = 'Password does not match';
+  }
+}
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   </head>
   <body>
   <?php include 'show-navbar.php'; ?>
@@ -23,14 +34,19 @@
       </div>
   </header>
   <br>
-  <form action="" method="post"> <!-- This button takes the user back to their email and also sends them a notification that their password has changed  -->
-    <label for="password">New password</label>
-    <br>
-    <input id="password" type="password" name="password" placeholder="New password" required>
-    <br>
-    <input id="password" type="password" name="password" placeholder="Repeat new password" required> <!-- check to make sure the new passwords are the same -->
-    <br>
-    <input type="submit" id="submit" value="submit"/>
-</form>
+
+  <form action="password_change_confirmation.php" method="post"> 
+  <label>New Password :
+  <input name="password" id="password1" type="password1" onkeyup='check();' placeholder="new password" required/>
+  </label>
+  <br>
+  <label>Confirm new password:
+  <input type="password1" name="password2" id="password2" onkeyup='check();' placeholder="confirm password" required/> 
+  </label>
+  <br>
+  <span id='message'></span>
+  <br>
+    <input type="submit" id="submit" value="Save password"/>
+  </form>
 
 </html>
