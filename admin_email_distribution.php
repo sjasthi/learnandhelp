ca3d180752a59e8bc85b4fbc13c2630845ca729d
schoolkolles <daniel.kolles@my.metrostate.edu>
2024-03-06 21:53:07 -0600

    Added Functionationality to support sending messages to multiple emails.  Content on Webpage is centered in table format per FP3 requirements.  Also added "Cancel" button to exit "User Email List" webpage to return back to "Admin" Page.

--- admin_email_distribution.php
+++ admin_email_distribution.php
@@ -4,130 +4,191 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-  $status = session_status();
-  if ($status == PHP_SESSION_NONE) {
-    session_start();
-  }
+$status = session_status();
+if ($status == PHP_SESSION_NONE) {
+  session_start();
+}
 
-  // Block unauthorized users from accessing the page
-  if (isset($_SESSION['role'])) {
-    if ($_SESSION['role'] != 'admin') {
-      http_response_code(403);
-      die('Forbidden');
-    }
-  } else {
+// Block unauthorized users from accessing the page
+if (isset($_SESSION['role'])) {
+  if ($_SESSION['role'] != 'admin') {
     http_response_code(403);
     die('Forbidden');
   }
- ?>
+} else {
+  http_response_code(403);
+  die('Forbidden');
+}
+?>
 
 <!DOCTYPE html>
 <script>
 </script>
 <html>
-  <head>
-    <link rel="icon" href="images/icon_logo.png" type="image/icon type">
-    <title>Administration email distribution</title>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;900&display=swap" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script>
 
-    $(document).ready(function () {
-      $('#Blog_table thead tr').clone(true).appendTo( '#Blog_table thead' );
-      $('#Blog_table thead tr:eq(1) th').each(function () {
-      var title = $(this).text();
-      $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+<head>
+  <link rel="icon" href="images/icon_logo.png" type="image/icon type">
+  <title>Administration email distribution</title>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;900&display=swap" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
+  <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('#Blog_table thead tr').clone(true).appendTo('#Blog_table thead');
+      $('#Blog_table thead tr:eq(1) th').each(function() {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder="Search ' + title + '" />');
       });
 
       var table = $('#Blog_table').DataTable({
-         initComplete: function () {
-             // Apply the search
-             this.api()
-                 .columns()
-                 .every(function () {
-                     var that = this;
+        initComplete: function() {
+          // Apply the search
+          this.api()
+            .columns()
+            .every(function() {
+              var that = this;
 
-                     $('input', this.header()).on('keyup change clear', function () {
-                         if (that.search() !== this.value) {
-                             that.search(this.value).draw();
-                         }
-                     });
-                 });
-             },
-         });
-
-      $('a.toggle-vis').on('click', function (e) {
-      e.preventDefault();
-
-      // Get the column API object
-      var column = table.column($(this).attr('data-column'));
-
-      // Toggle the visibility
-      column.visible(!column.visible());
+              $('input', this.header()).on('keyup change clear', function() {
+                if (that.search() !== this.value) {
+                  that.search(this.value).draw();
+                }
+              });
+            });
+        },
       });
-     });
-    </script>
-  </head>
-  <body>
+
+      $('a.toggle-vis').on('click', function(e) {
+        e.preventDefault();
+
+        // Get the column API object
+        var column = table.column($(this).attr('data-column'));
+
+        // Toggle the visibility
+        column.visible(!column.visible());
+      });
+    });
+  </script>
+</head>
+
+<body>
   <?php include 'show-navbar.php'; ?>
   <?php show_navbar(); ?>
-    <header class="inverse">
-      <div class="container">
-        <h1><span class="accent-text">Users Email List</span></h1>
-      </div>
-    </header>
+  <header class="inverse">
+    <div class="container">
+      <h1><span class="accent-text">Users Email List</span></h1>
+    </div>
+  </header>
 
-    <div style="padding-top: 10px; padding-bottom: 30px; width:90%; margin:auto; overflow:auto">
+  <div style="padding-top: 10px; padding-bottom: 30px; width:90%; margin:auto; overflow:auto">
 
-      <html lang="en" dir="ltr">
-          <head>
-              <meta charset="utf-8">
-              <title>Send Email</title>
-          </head>
-          <body>
-              <form class="" action="send.php" method="post">
-              From: <textarea style="height:20px; width:500px" type="email" name="from"><?php
+    <html lang="en" dir="ltr">
 
-              ?></textarea><br>
-              Email: <input style="height:20px; width:500px" type="email" name="email" placeholder="recipients" value=""> <br>
-              Subject: <input style="height:20px; width:500px" type="text" name="subject" value="" placeholder='Enter a subject...'> <br>
-              <label> Message: </lable>
-              <textarea type="text" style="height:200px; width:500px" name="message" value="" placeholder='Enter a message...' row='10'></textarea> <br>
-              <button type="submit" name="send">Send</button>
-              <button type="reset" value="Reset">Cancel</button> <br>
-              <label>Email List:</lable> <br>
-              <textarea rows="10" cols="140"> <?php
-                // Connect to your database
-                                $db_host = "localhost";
-                                $db_name = "learn_and_help_db";
-                                $db_user = "root";
-                                $db_pass = "";
+    <head>
+      <meta charset="utf-8">
+      <title>Send Email</title>
+    </head>
 
-                                $conn = mysqli_connect ($db_host, $db_user, $db_pass, $db_name);
-                                if(mysqli_connect_error()){
-                                    echo mysqli_connect_error();
-                                    exit;
-                                }
+    <body>
+      <table style="margin-left:auto; margin-right:auto">
+        <tr>
+          <form class="" action="send.php" method="post">
+        <tr>
+          <td>
+            <label>From Admin:</label>
+          </td>
+          <td>
+            <input style="height:20px; width:500px" type="email" name="from" value="" required></input><br>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>Email:</label>
+          </td>
+          <td>
+            <textarea style="height:auto; width:500px;" type="text" name="email" placeholder="recipients" value="" required>
+        <?php
+        // Connect to your database
+        $db_host = "localhost";
+        $db_name = "learn_and_help_db";
+        $db_user = "root";
+        $db_pass = "";
 
-                                $sql = "SELECT Email FROM users";
-                                $result = $conn->query($sql);
+        $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+        if (mysqli_connect_error()) {
+          echo mysqli_connect_error();
+          exit;
+        }
 
-                                if ($result->num_rows > 0) {
-                                    // Output data in the email row
-                                    while($row = $result->fetch_assoc()) {
-                                        echo $row["Email"] . "; ";
-                                    }
-                                } else {
-                                    echo "0 results";
-                                }
+        $sql = "SELECT Email FROM users";
+        $result = $conn->query($sql);
 
-                                $conn->close();
-              ?></textarea>
-              </form>
-          </body>
-      </html>
-  </body>
+
+        if ($result->num_rows > 0) {  //Grab all email address and populate Email field
+
+          $rowCount = $result->num_rows;   //Get total # of Emails found
+          $loops = 1;
+
+          // Output data in the email field in form.  Creates email list separated by ';'.
+          while ($row = $result->fetch_assoc()) {
+
+            if ($loops == $rowCount) {
+              echo $row["Email"]; // omit comma at end.
+            } else {
+              echo $row["Email"] . "; "; // Add ; to list
+            }
+            $loops += 1;  //increment loop count
+
+          }
+        } else {
+          echo "0 results";
+        }
+
+
+
+        $conn->close();
+
+        ?></textarea><br>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>Subject:</label>
+          </td>
+          <td>
+            <input style="height:20px; width:500px" type="text" name="subject" value="" placeholder='Enter a subject...' required> <br>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>Message:</label>
+          </td>
+          <td>
+            <textarea type="text" style="height:200px; width:500px" name="message" value="" placeholder='Enter a message...' row='10' required></textarea><br><br>
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>
+            <button class=btn type="submit" name="send" style="display:block; margin:auto">Send</button><br>
+          </td>
+        </tr>
+        </form>
+        </tr>
+        <tr>
+          <td></td>
+          <td>
+            <form action="administration.php" method="post">
+              <button class=btn type="submit" name="cancel" id="submit" style="display:block; margin:auto">Cancel</button>
+          </td>
+        </tr>
+        </form>
+        </tr>
+      </table>
+    </body>
+
+    </html>
+</body>
+
 </html>
--- send.php
+++ send.php
@@ -1,32 +1,49 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
 
-    require 'PHPMailer-master/src/Exception.php';
-    require 'PHPMailer-master/src/PHPMailer.php';
-    require 'PHPMailer-master/src/SMTP.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    if(isset($_POST['send'])){
-        $mail = new PHPMailer(true);
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
 
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'mekics499project24@gmail.com'; // your gamil, substitute with admin email
-        $mail->Password = 'fwlphiafqwbzkubj'; // your gmail app pass
-        $mail->SMTPSecure = 'ssl';
-        $mail->Port = 465;
 
-        $mail->setFrom('mekics499project24@gmail.com'); // your gmail
+if (isset($_POST['send'])) {
 
-        $mail->addAddress($_POST["email"]);
-        $mail->isHTML(true);
+    if (isset($_POST['email'])) {
+        $emails = $_POST['email'];  // reads in the comma delimited list from email field
+        $emailArray = explode('; ', $emails);   // Converts the list to an array
 
-        $mail->Subject = $_POST["subject"];
-        $mail->Body = $_POST["message"];
+        $mail = new PHPMailer(true);        //Get new instance of the class for PHMailer()
 
-        $mail->send();
 
+        // print "Values from email list: <br>";
+        for ($i = 0; $i < count($emailArray); $i++) {
+            // print "$emailArray[$i]<br>";  //debug line -- Remove when not needed.
+
+
+            // $mail = new PHPMailer(true);
+
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'mekics499project24@gmail.com'; // your gmail, substitute with admin email
+            $mail->Password = 'fwlphiafqwbzkubj'; // your gmail app pass
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+
+            $mail->setFrom('mekics499project24@gmail.com'); // your gmail
+
+            $mail->addAddress($emailArray[$i]);   // Get email from array segment
+
+            $mail->isHTML(true);
+
+            $mail->Subject = $_POST["subject"];
+            $mail->Body = $_POST["message"];
+
+            $mail->send();
+        }
+        // Report sending of all emails is complete.
         echo
         "
         <script>
@@ -34,6 +51,11 @@
         document.location.href = 'admin_email_distribution.php'
         </script>
         ";
+    } else {  // 
+        $emails = array();
     }
 
-?>
+    // $emails = $_POST['email'];
+    // print "<br>";
+    // print_r($emails);
+}
