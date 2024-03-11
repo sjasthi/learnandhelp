0b8eb1498a3d72fa6e98047e82af098183e3eeb2
Laoj25 <nc8612qg@go.minnstate.edu>
2024-03-11 18:09:25 -0500

    update new password to database created by user

--- new_password_entry.php
+++ new_password_entry.php
@@ -3,7 +3,56 @@
   if ($status == PHP_SESSION_NONE) {
     session_start();
   }
- ?>
+
+      // Include the database configuration file
+      require 'db_configuration.php';
+
+      $connection = mysqli_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
+
+      // Check if the connection is established
+      if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+      }
+
+      // Retrieve the User_ID from the query parameter
+      if (isset($_POST['usermail'])) {
+        $email = $_POST['usermail'];
+      } else {
+        // Handle the case where the User_ID is not provided in the URL
+        // You can set a default value or handle it as needed
+        $email = '';
+      }
+
+      // Check if the form is submitted
+      if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Retrieve form data
+        $password = $_POST['password1']; // Retrieve the new password
+        $hash = sha1($password); // Hash the password
+
+        // Update the user's password in the database
+        $updateQuery = "UPDATE users
+                        SET
+                            Hash = '$hash'
+                        WHERE
+                            Email = '$email'";
+
+        // Perform the query
+        $updateResult = mysqli_query($connection, $updateQuery);
+
+        // Check if the query was successful
+        if ($updateResult) {
+          // Redirect back to the main PHP file or any desired page after successful update
+          header("Location: admin_usersList.php");
+          exit;
+        } else {
+          // If there was an error, display the error message
+          echo "Error updating password: " . mysqli_error($connection);
+        }
+      }
+
+      // Close the database connection (you should do this at the end of your PHP script)
+      mysqli_close($connection);
+    ?>
  
 <!DOCTYPE html>
 <html lang="en">
@@ -49,10 +98,10 @@
       </div>
   </header>
   <br>
-  <form action="password_change_confirmation.php" method="post"> 
+  <form action="password_change_confirmation.php" method="post">
       <input type="password" name="password1" id="password1"  onkeyup='check();' placeholder="New password" style="width:400px" required/>
       <br>
-      <input type="password" name="password2" id="password2" onkeyup='check();' placeholder="Repeat new password" style="width:400px" required/> 
+      <input type="password" name="password2" id="password2" onkeyup='check();' placeholder="Repeat new password" style="width:400px" required/>
       <br>
       <input type="checkbox" onclick="showPassword()">Show Password
       <br>
