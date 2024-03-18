1df712cb98da6a5652295546583b5a683fc1d7d5
ChampionsICS370 <mekigebr21@gmail.com>
2024-03-18 16:48:52 -0500

    FP5
    Here are contact us page, and form send email files

--- contact_us.php
+++ contact_us.php
@@ -8,21 +8,59 @@
 <!DOCTYPE html>
 <html>
   <head>
+   <title>Align Content to Left</title>
     <link rel="icon" href="images/icon_logo.png" type="image/icon type">
     <title>Learn and Help</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;900&display=swap" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
+
+    <style>
+      .content-container {
+       position: absolute;
+       text-align: left;
+       top: 70%; 
+       left: 20px; 
+       transform: translateY(-50%); 
+       padding: 20px;
+       border: 1px solid #ccc;
+       border-radius: 5px;
+    }
+
+
+
+    .top-right-container {
+      position: absolute;
+      top: 70%;
+      right: 0;
+      transform: translateY(-50%); 
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+  }
+</style>
+  
   </head>
   <body>
   <?php include 'show-navbar.php'; ?>
   <?php show_navbar(); ?>
+
+  
   <header class="inverse">
+    
       <div class="container">
-        <h1> <span class="accent-text">Contact Us</span></h1>
+        <h1> <span class="accent-text">About</span></h1>
       </div>
   </header>
     <section class="about-me">
       <div class="container">
+      <div class="input-row">
+      <div class="content-container">
+  
+      
+       
+                   <a href = "form_send_email.php" >Send Email</a>
+                   
+             </div>
         <h2><span class="accent-text">Any Questions?</span></h2>
         <p>Please contact <strong>Siva Jasthi</strong></p>
         <a href="mailto: Siva.Jasthi@gmail.com"> Siva.Jasthi@gmail.com</a>
@@ -36,6 +74,17 @@
         <p><b>William Vicic</b>: william.vicic@gmail.com</p>
         <p><b>Seth Arndt</b>: 17seth.arndt@gmail.com</p>
         <p><b>Robert LaPrise</b>: lapriserobert1@gmail.com</p>
+
+        </section>
+    <section class="about-me2">
+      <div class="container22">
+        <div class="info-box">
+
+          <h2><span class="accent-text">Contact Information </span></h2>
+          <p>Contact Email: siva.jasthi@gmail.com</p>
+          <p><a href="http://localhost/learnandhelp.php">Learn and Help PDF</a></p>
+
+         
       </div>
     </section>
   </body>
