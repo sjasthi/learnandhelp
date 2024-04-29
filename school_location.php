e0f9c7cbfa8fb173d03b17789d08546e5269af4b
schoolkolles <daniel.kolles@my.metrostate.edu>
2024-04-29 15:02:00 -0500

    Updated school locations with latest csv file from Jasthi dated 4-26-24

--- school_location.php
+++ school_location.php
@@ -1,31 +1,29 @@
 <?php
 
 $status = session_status();
-  if ($status == PHP_SESSION_NONE) {
+if ($status == PHP_SESSION_NONE) {
     session_start();
-  }
-             require 'db_configuration.php';
-            // Create connection
-                      $conn = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
+}
+require 'db_configuration.php';
+// Create connection
+$conn = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
 
-                      // Check connection
-                      if ($conn->connect_error)
-                      {
-                        die("Connection failed: " . $conn->connect_error);
-                      }
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-                      $sql = "SELECT * FROM `schools`;";
-                      $result = $conn->query($sql);
+$sql = "SELECT * FROM `schools`;";
+$result = $conn->query($sql);
 
-                        if ($result->num_rows > 0){
-                            // Create table with data from each row
-                            while($row = $result->fetch_assoc()){
-                                $pin = $row['pin_code'];
-
-                            }
-                        }
-            $conn->close();
-    ?>
+if ($result->num_rows > 0) {
+    // Create table with data from each row
+    while ($row = $result->fetch_assoc()) {
+        $pin = $row['pin_code'];
+    }
+}
+$conn->close();
+?>
 
 
 <!DOCTYPE html>
@@ -45,13 +43,15 @@ $status = session_status();
             position: relative;
             overflow: hidden;
         }
-        
+
         .google-map iframe {
             position: absolute;
-            top: -100px; /* Adjust top position to hide the black bars */
+            top: -100px;
+            /* Adjust top position to hide the black bars */
             left: 0;
             width: 100%;
-            height: calc(100% + 200px); /* Adjust height to include the black bars */
+            height: calc(100% + 200px);
+            /* Adjust height to include the black bars */
             border: 0;
         }
     </style>
@@ -65,9 +65,10 @@ $status = session_status();
             <h1><span class="accent-text">School location</span></h1>
         </div>
     </header>
-    <br>
+    <!-- <br> -->
     <div class="google-map">
-        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=15Z3ti2e87K_gqlgufm7YPgjBNI-g2Pw&ehbc=2E312F" width="100%" height="100%" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <!-- <iframe src="https://www.google.com/maps/d/u/0/embed?mid=15Z3ti2e87K_gqlgufm7YPgjBNI-g2Pw&ehbc=2E312F" width="100%" height="100%" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> -->
+        <iframe src="https://www.google.com/maps/d/embed?mid=1YO3KMnho2KyJWY76ZTpYK1NEtFFgb5w&hl=en&ehbc=2E312F" width="100%" height="100%" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     </div>
 </body>
 
