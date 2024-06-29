0f2decd7fc222233e36f02b690d8ea8effe9cf3e
doing-computer-stuff <118572359+doing-computer-stuff@users.noreply.github.com>
2024-06-29 17:21:51 -0500

    validate-login, registration_form, form-submit updated
    registration details now appear in an accordion list
    after login this page shows
    the list options change based on whether or not the user is currently registered for a class
    need to implement the logic for showing prior registrations in the list and editing current class registration if exists

--- form-submit.php
+++ form-submit.php
@@ -21,7 +21,7 @@ if (isset($_POST['action'])) {
 } else {
 	$action = '';
 }
-
+$User_Id = $_SESSION['User_Id'];
 if ($action == 'edit' || $action == 'add' || $action == 'admin_edit') {
     // Validate and sanitize form input
     $sponsor_name = isset($_POST['sponsers-name']) ? htmlspecialchars($_POST['sponsers-name']) : '';
@@ -38,7 +38,6 @@ if ($action == 'edit' || $action == 'add' || $action == 'admin_edit') {
 
     $timestamp = date("Y-m-d H:i:s");
 } else {
-		$User_Id = $_SESSION['User_Id'];
     $sql = "SELECT * FROM registrations NATURAL JOIN classes NATURAL JOIN user_registrations WHERE User_Id = $User_Id";
     $row = mysqli_fetch_array(mysqli_query($connection, $sql));
 
@@ -182,29 +181,31 @@ echo "<!DOCTYPE html>
 		<h3> Registration Details </h3>
     <div id=\"container_2\">
 		<form action=\"registration_edit.php\" method = \"post\">
-				<input type='hidden' name='Reg_Id' value=$Reg_Id>
-        <!---Sponsors Section -->
+				<input type='hidden' name='Reg_Id' value=$Reg_Id>";
+
+	if (!empty($sponsor_name)) {
+        echo "<!---Sponsors Section -->
         <label id=\"name-label\"><b>Sponsor's Name:</b> $sponsor_name</label><br>
         <input type=\"hidden\" id=\"action\" name=\"action\" value=\"edit\">
         <label id=\"sponsers-email-label\"> <b>Sponsor's Email:</b> $sponsor_email</label><br>
-		<label id=\"sponsers-number-label\"><b>Sponsor's Phone Number:</b> $sponsor_phone</label><br>
-        <input type=\"hidden\" id=\"sponsers-name\" name=\"sponsers-name\" class=\"form\" value=\"$sponsor_name\"><!--name--->
-		<input type=\"hidden\" id=\"sponsers-email\" name=\"sponsers-email\" class=\"form\" value=\"$sponsor_email\"><br><!---email-->
-        <input type=\"hidden\" id=\"sponsers-phone\" name=\"sponsers-phone\" value=\"$sponsor_phone\">
-
-        <br>
-        <!---Spouse Section -->
+		<label id=\"sponsors-number-label\"><b>Sponsor's Phone Number:</b> $sponsor_phone</label><br>";
+	}
+     echo "<input type=\"hidden\" id=\"sponsers-name\" name=\"sponsers-name\" class=\"form\" value=\"$sponsor_name\"><!--name--->
+		<input type=\"hidden\" id=\"sponsers-email\" name=\"sponsers-email\" class=\"form\" value=\"$sponsor_email\"><!---email-->
+        <input type=\"hidden\" id=\"sponsers-phone\" name=\"sponsers-phone\" value=\"$sponsor_phone\"><br>";
+		
+	if (!empty($spouse_name)) {
+        echo "<!---Spouse Section -->
         <label id=\"spouses-name-label\"><b>Spouse's Name:</b> $spouse_name</label><br>
 
         <label id=\"spouses-email-label\"> <b>Spouse's Email:</b> $spouse_email</label><br>
 
-        <label id=\"spouses-number-label\"><b>Spouse's Phone Number:</b> $spouse_phone</label><br>
-		<input type=\"hidden\" id=\"spouses-name\" name=\"spouses-name\" class=\"form\" value=\"$spouse_name\"><!--name--->
-		<input type=\"hidden\" id=\"spouses-email\" name=\"spouses-email\" class=\"form\" value=\"$spouse_email\"><br><!---email-->
+        <label id=\"spouses-number-label\"><b>Spouse's Phone Number:</b> $spouse_phone</label><br><br>";
+	}
+	echo "<input type=\"hidden\" id=\"spouses-name\" name=\"spouses-name\" class=\"form\" value=\"$spouse_name\"><!--name--->
+		<input type=\"hidden\" id=\"spouses-email\" name=\"spouses-email\" class=\"form\" value=\"$spouse_email\"><!---email-->
         <input type=\"hidden\" id=\"spouses-phone\" name=\"spouses-phone\" value=\"$spouse_phone\">
-
-        <br>
-        <!---Student Section -->
+	    <!---Student Section -->
         <label id=\"students-name-label\"><b>Student's Name:</b> $student_name</label><br>
 
         <label id=\"students-email-label\"><b>Student's Email:</b> $student_email</label><br>
@@ -214,12 +215,13 @@ echo "<!DOCTYPE html>
 		<input type=\"hidden\" id=\"students-email\" name=\"students-email\" class=\"form\" value=\"$student_email\"><br><!---email-->
         <input type=\"hidden\" id=\"students-phone\" name=\"students-phone\" value=\"$student_phone\">
 
-        <br>
         <label id=\"class\"><b>Selected Class:</b> $class</label><br>
 		<input type=\"hidden\" id=\"class\" name=\"class\" value=\"$class\">
-		<!--dropdown--->
-		<p><b>Cause:</b> $cause</p><br>
-		<input type=\"hidden\" id=\"cause\" name=\"cause\" value=\"$cause\">
+		<!--dropdown--->";
+	if (!empty($cause)) {
+		echo "<p><b>Cause:</b> $cause</p>";
+	}
+	echo "<input type=\"hidden\" id=\"cause\" name=\"cause\" value=\"$cause\">
 		<br>
 		<input type='hidden' name='action' value='edit'>
 		<input type=\"submit\" id=\"submit-registration\" name=\"submit\" value=\"Edit\"></a>
--- registration_form.php
+++ registration_form.php
@@ -18,9 +18,9 @@ if (isset($_SESSION['User_Id'])) {
 
   $result = $connection->query($sql);
 
-  if ($result->num_rows > 0) {
-    header("Location: form-submit.php");
-  }
+//  if ($result->num_rows > 0) {
+//   header("Location: form-submit.php");
+//  }
   
   //Find user's information to populate.
   $sql = 'SELECT * FROM users WHERE User_Id = ' . $User_Id . ';';
@@ -42,6 +42,12 @@ if (isset($_SESSION['User_Id'])) {
   header("Location: login.php");
 }
 
+//get active registration year
+$active_reg_query = "SELECT value FROM preferences WHERE name = 'ACTIVE_REGISTRATION';";
+$active_reg_result = $connection->query($active_reg_query);
+$active_reg_array = $active_reg_result->fetch_assoc();
+$active_reg = $active_reg_array["value"];
+
 include 'show-navbar.php';
 
 echo "<!DOCTYPE html>
@@ -51,113 +57,211 @@ echo "<!DOCTYPE html>
     <title>Learn and Help</title>
     <link href=\"https://fonts.googleapis.com/css2?family=Roboto:wght@300;900&display=swap\" rel=\"stylesheet\">
     <link href=\"css/main.css\" rel=\"stylesheet\">
-  </head>
-  <body>";
-show_navbar();
-echo      "<header class=\"inverse\">
-          <div class=\"container\">
-              <h1><span class=\"accent-text\">Register Now</span></h1>
-          </div>
-          </header>
-      <h3> Registration Form</h3>
-      <p><i>* Required Fields</i></p>
-	  <form id=\"survey-form\" action=\"form-submit.php\" method = \"post\">
-      <div id= \"container_2\">
-        <!---Student Section -->
-        <label id=\"students-name-label\">*Student's Name: </label>
-        <input type=\"text\" id=\"students-name\" name=\"students-name\" class=\"form\" required placeholder=\"Enter Student's name\" value=\"$fullname\"><br>
-        <label id=\"students-email-label\"> *Student's Email: </label>
-        <input type=\"email\" id=\"students-email\" name=\"students-email\" class=\"form\" required placeholder=\"Enter Student's email\" value=\"$email\"><br>
-        <label id=\"students-number-label\">*Student's Phone Number: </label>
-        <input type=\"tel\" id=\"students-phone\" name=\"students-phone\" placeholder=\"123-456-7899\" value=\"$phone\" pattern=\"[0-9]{3}-[0-9]{3}-[0-9]{4}\" required>
-        <br>
-        <br>
-        <label id=\"class\">*Select Class: </label>
-        <select id=\"dropdown\" name=\"class\" required>"; // changed name to class
-
-
-// Select view of available classes for users from accessing the page 
-// Admin's can see all classes regardless of status
-if ((isset($_SESSION['email'])) &&  $_SESSION['role'] == 'admin') {
-  $class_query = "SELECT Class_Id, Class_Name, Description, Status
-              FROM classes;";
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+<style>
+.accordion {
+  background-color: #eee;
+  color: #444;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: center;
+  outline: none;
+  font-size: 20px;
+  transition: 0.4s;
 }
 
-//Non-Admin's and users not logged in can only see "Approved" Classes
-else {
-	$active_reg_query = "SELECT value FROM preferences WHERE name = 'ACTIVE_REGISTRATION';";
-	$active_reg_result = $connection->query($active_reg_query);
-	$active_reg_array = $active_reg_result->fetch_assoc();
-	$active_reg = $active_reg_array["value"];
-	
-	$offerings_query = "SELECT Class_Id FROM offerings WHERE batch = '$active_reg';";
-	$offerings_result = $connection->query($offerings_query);
-	
-	$class_id_list = "";
-	if($offerings_result->num_rows > 0)
-	{
-		$i = 0;
-		while($offerings_row = $offerings_result->fetch_assoc())
+.active, .accordion:hover {
+  background-color: #ccc; 
+}
+
+.panel {
+  padding: 0 18px;
+  display: none;
+  background-color: white;
+  overflow: hidden;
+}
+</style>
+</head>
+<body>";
+show_navbar();
+echo "<header class=\"inverse\">
+	<div class=\"container\">
+	<h1><span class=\"accent-text\">Register Now</span></h1>
+	</div>
+	</header>
+	<h2><strong>Registration Details</strong></h2>";
+//get list of current and prior class registrations for user, if they exist
+$user_reg_query = "SELECT Class_Id FROM registrations WHERE Student_Name = '". $fullname. "'";
+$user_reg_result = $connection->query($user_reg_query);
+$is_registered = false;
+if($user_reg_result->num_rows > 0) {
+	$user_reg_array = $user_reg_result->fetch_assoc();
+	$is_registered = true;
+}
+//if user isn't registered for a class this term, show registration form
+//TODO need to add functionality for displaying any past registrations
+if (!$is_registered) {
+	echo "<button class='accordion'>$active_reg</button>
+		<div class='panel'>
+		<p>You are not currently registered for this academic year.</p>
+		<p>Enter your registration details.</p>
+		<h3> Registration Form</h3>
+		<p><i>* Required Fields</i></p>
+		<form id=\"survey-form\" action=\"form-submit.php\" method = \"post\">
+		<div id= \"container_2\">
+		<!---Student Section -->
+		<label id=\"students-name-label\">*Student's Name: </label>
+		<input type=\"text\" id=\"students-name\" name=\"students-name\" class=\"form\" required placeholder=\"Enter Student's name\" value=\"$fullname\"><br>
+		<label id=\"students-email-label\"> *Student's Email: </label>
+		<input type=\"email\" id=\"students-email\" name=\"students-email\" class=\"form\" required placeholder=\"Enter Student's email\" value=\"$email\"><br>
+		<label id=\"students-number-label\">*Student's Phone Number: </label>
+		<input type=\"tel\" id=\"students-phone\" name=\"students-phone\" placeholder=\"123-456-7899\" value=\"$phone\" pattern=\"[0-9]{3}-[0-9]{3}-[0-9]{4}\" required>
+		<br>
+		<br>
+		<label id=\"class\">*Select Class: </label>
+		<select id=\"dropdown\" name=\"class\" required>";
+	// Select view of available classes for users from accessing the page 
+	// Admin's can see all classes regardless of status
+	if ((isset($_SESSION['email'])) &&  $_SESSION['role'] == 'admin') {
+	  $class_query = "SELECT Class_Id, Class_Name, Description, Status FROM classes;";
+	}
+	//Non-Admin's and users not logged in can only see "Approved" Classes
+	else {
+		$active_reg_query = "SELECT value FROM preferences WHERE name = 'ACTIVE_REGISTRATION';";
+		$active_reg_result = $connection->query($active_reg_query);
+		$active_reg_array = $active_reg_result->fetch_assoc();
+		$active_reg = $active_reg_array["value"];
+		
+		$offerings_query = "SELECT Class_Id FROM offerings WHERE batch = '$active_reg';";
+		$offerings_result = $connection->query($offerings_query);
+		
+		$class_id_list = "";
+		if($offerings_result->num_rows > 0)
 		{
-			$class_id_list .= strval($offerings_row["Class_Id"]);
-			$i++;
-			if($i < $offerings_result->num_rows){
-				$class_id_list .= ", ";
+			$i = 0;
+			while($offerings_row = $offerings_result->fetch_assoc())
+			{
+				$class_id_list .= strval($offerings_row["Class_Id"]);
+				$i++;
+				if($i < $offerings_result->num_rows){
+					$class_id_list .= ", ";
+				}
 			}
 		}
-	}
-	
-	$class_query = "SELECT Class_Id, Class_Name, Description, Status FROM classes WHERE Class_Id IN ($class_id_list)";
-	echo $class_query;
-}
-
-
-// Fetch classes from the database
-//$class_query = "SELECT * FROM classes";
-$class_result = $connection->query($class_query);
-if (!$class_result) {
-  echo "Error: " . $connection->error;
-} else {
-  if ($class_result->num_rows > 0) {
-    while ($row = $class_result->fetch_assoc()) {
-      echo "<option value=\"" . $row["Class_Id"] . "\">" . $row["Class_Name"] . "</option>";
-    }
-  } else {
-    echo "<option disabled selected value>No classes found</option>";
-  }
-}
-
-echo      "</select>
-		<!--dropdown--->
 		
-		</label><!---radioButtons--->
-		<br>
-    </div>
+		$class_query = "SELECT Class_Id, Class_Name, Description, Status FROM classes WHERE Class_Id IN ($class_id_list)";
+	}
 
-    <div id=\"right\">
-      
-        <!---Sponsors Section -->
-        <label id=\"name-label\">Sponsor's Name: </label>
-        <input type=\"text\" id=\"sponsers-name\" name=\"sponsers-name\" class=\"form\" placeholder=\"Enter Sponsor's name\"><br><!--name--->
-        <label id=\"sponsers-email-label\"> Sponsor's Email: </label>
-        <input type=\"email\" id=\"sponsers-email\" name=\"sponsers-email\" class=\"form\" placeholder=\"Enter Sponsor's email\"><br><!---email-->
-        <label id=\"sponsers-number-label\">Sponsor's Phone Number: </label>
-        <input type=\"tel\" id=\"sponsers-phone\" name=\"sponsers-phone\" placeholder=\"123-456-7899\" pattern=\"[0-9]{3}-[0-9]{3}-[0-9]{4}\">
-        <br>
-        <br>
-        <!---Spouse Section -->
-        <label id=\"spouses-name-label\">Spouse's Name: </label>
-        <input type=\"text\" id=\"spouses-name\" name=\"spouses-name\" class=\"form\" placeholder=\"Enter Spouse's name\"><br>
-        <label id=\"spouses-email-label\"> Spouse's Email: </label>
-        <input type=\"email\" id=\"spouses-email\" name=\"spouses-email\" class=\"form\" placeholder=\"Enter Spouse's email\"><br>
-        <label id=\"spouses-number-label\">Spouse's Phone Number: </label>
-        <input type=\"tel\" id=\"spouses-phone\" name=\"spouses-phone\" placeholder=\"123-456-7899\" pattern=\"[0-9]{3}-[0-9]{3}-[0-9]{4}\">
 
-      </div>
-<br>
-    <input type=\"submit\" id=\"submit-registration\" name=\"submit\" value=\"Submit\">
-    <input type=\"hidden\" id=\"action\" name=\"action\" value=\"add\">
-    </form><!---survey-form--->
+	// Fetch classes from the database
+	//$class_query = "SELECT * FROM classes";
+	$class_result = $connection->query($class_query);
+	if (!$class_result) {
+	  echo "Error: " . $connection->error;
+	} 
+	else {
+	  if ($class_result->num_rows > 0) {
+		while ($row = $class_result->fetch_assoc())
+			echo "<option value=\"" . $row["Class_Id"] . "\">" . $row["Class_Name"] . "</option>";
+	  } 
+	  else {
+			echo "<option disabled selected value>No classes found</option>";
+	  }
+	}
+	mysqli_free_result($class_result);
+	echo "</select>
+	<!--dropdown--->
+	
+	</label><!---radioButtons--->
+	<br>
+	</div>
 
-  </body>
-</html>";
+	<div id=\"right\">
+  
+	<!---Sponsors Section -->
+	<label id=\"name-label\">Sponsor's Name: </label>
+	<input type=\"text\" id=\"sponsers-name\" name=\"sponsers-name\" class=\"form\" placeholder=\"Enter Sponsor's name\"><br><!--name--->
+	<label id=\"sponsers-email-label\"> Sponsor's Email: </label>
+	<input type=\"email\" id=\"sponsers-email\" name=\"sponsers-email\" class=\"form\" placeholder=\"Enter Sponsor's email\"><br><!---email-->
+	<label id=\"sponsers-number-label\">Sponsor's Phone Number: </label>
+	<input type=\"tel\" id=\"sponsers-phone\" name=\"sponsers-phone\" placeholder=\"123-456-7899\" pattern=\"[0-9]{3}-[0-9]{3}-[0-9]{4}\">
+	<br>
+	<br>
+	<!---Spouse Section -->
+	<label id=\"spouses-name-label\">Spouse's Name: </label>
+	<input type=\"text\" id=\"spouses-name\" name=\"spouses-name\" class=\"form\" placeholder=\"Enter Spouse's name\"><br>
+	<label id=\"spouses-email-label\"> Spouse's Email: </label>
+	<input type=\"email\" id=\"spouses-email\" name=\"spouses-email\" class=\"form\" placeholder=\"Enter Spouse's email\"><br>
+	<label id=\"spouses-number-label\">Spouse's Phone Number: </label>
+	<input type=\"tel\" id=\"spouses-phone\" name=\"spouses-phone\" placeholder=\"123-456-7899\" pattern=\"[0-9]{3}-[0-9]{3}-[0-9]{4}\">
+
+	 </div>
+	<br>
+	<input type=\"submit\" id=\"submit-registration\" name=\"submit\" value=\"Submit\">
+	<input type=\"hidden\" id=\"action\" name=\"action\" value=\"add\">
+	</form><!---survey-form--->
+	</div>
+
+	<script>
+	var acc = document.getElementsByClassName('accordion');
+	var i;
+	for (i = 0; i < acc.length; i++) {
+	  acc[i].addEventListener('click', function() {
+		this.classList.toggle('active');
+		var panel = this.nextElementSibling;
+		if (panel.style.display === 'block') {
+		  panel.style.display = 'none';
+		} else {
+		  panel.style.display = 'block';
+		}
+	  });
+	}
+	</script>";
+}
+//TODO
+//else display current registration, and past registration if exists
+//also link to registration_edit.php somehow
+else {
+	$class_id = $user_reg_array['Class_Id'];
+	$class_name_query = "SELECT Class_Name FROM classes WHERE Class_Id = '$class_id';";
+	$class_name_result = $connection->query($class_name_query);
+	$class_name_array = $class_name_result->fetch_assoc();
+	mysqli_free_result($class_name_result);
+	$class_name = $class_name_array['Class_Name'];
+	echo "<button class='accordion'>$active_reg</button>
+		<div class='panel'>
+		<p><strong>Registration details for $active_reg.</strong></p>
+		<p>$class_name</p><br>
+		
+		</div>
+		
+		<button class='accordion'>Previous Year</button>
+		<div class='panel'>
+		<p>Enrollment for previous year if exists.</p><br>
+		</div>
+		
+		<button class='accordion'>Previous Year</button>
+		<div class='panel'>
+		<p>Enrollment for previous year if exists.</p><br>
+		</div>
+		
+		<script>
+		var acc = document.getElementsByClassName('accordion');
+		var i;
+		for (i = 0; i < acc.length; i++) {
+		  acc[i].addEventListener('click', function() {
+			this.classList.toggle('active');
+			var panel = this.nextElementSibling;
+			if (panel.style.display === 'block') {
+			  panel.style.display = 'none';
+			} else {
+			  panel.style.display = 'block';
+			}
+		  });
+		}
+		</script>";
+}
+echo "</body>
+	 </html>";
+?>
