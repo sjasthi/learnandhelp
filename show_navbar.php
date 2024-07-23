350cdf249f4a19a0469d2ca6bc461d3da42af19a
albM23 <js4585dz@go.minnstate.edu>
2024-07-23 17:37:37 -0500

    Add files via upload

--- show_navbar.php
+++ show_navbar.php
@@ -4,8 +4,8 @@ include 'show-button.php';
 function show_navbar() {
     echo '<div class="navbar">
       <a href="index.php" id="nav-logo"><img id="logo" src="images/learn_n_help_logo.png"></a>
-	  <div>'; getButton(); echo '</div>
-	  <a href=schools.php>Schools</a>
+      <div>'; getButton(); echo '</div>
+      <a href="schools.php">Schools</a>
       <a href="classes.php">Classes</a>
       <a href="books.php">Books</a>
       <a href="books_grid.php">Books(Grid)</a>
@@ -13,23 +13,17 @@ function show_navbar() {
       <a href="blog.php">Blog</a>
       <a href="meet_our_instructors.php">Instructors</a>
       <a href="suggest_school.php">Suggest a School</a>
-      <a href="contact_us.php">Contact Us</a>';
+      <a href="contact_us.php">Contact Us</a>
+      <a href="quote_of_the_day.php" id="quote-link">Quote of the Day</a>';
 
-
-      
-      
-    
-      
-      if (isset($_SESSION['email'])) {
+    if (isset($_SESSION['email'])) {
         if ($_SESSION['role'] == 'admin') {
-          echo '<a href="administration.php">Administration</a>';
+            echo '<a href="administration.php">Administration</a>';
         }
         echo '<a href="registration_form.php" id="register">Register Now</a>';
-      }elseif(isset($_SESSION['email']) == false){
+    } elseif (!isset($_SESSION['email'])) {
         echo '<a href="login.php" id="register">Register Now</a>';
-      }
-       echo '</div>';
-
+    }
+    echo '</div>';
 }
-
 ?>
